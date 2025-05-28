ee156b03ca202d02acaa9b60105ffb57fcd9364a
sefmax <sefmax7@gmail.com>
2025-05-28 23:36:48 +0300

    fixed

--- home_work2/home_work2.py
+++ home_work2/home_work2.py
@@ -7,5 +7,6 @@ numb2 = numb // 10 % 10
 numb3 = numb // 100 % 10
 numb4 = numb // 1000 % 10
 numb5 = numb // 10000 % 10
+reverse_numb = numb1 * 10000 + numb2 * 1000 + numb3 * 100 + numb4 * 10 + numb5 * 1
 
-print(numb1, numb2, numb3, numb4, numb5)
+print(reverse_numb)
